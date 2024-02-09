81d8145583418c4aeb46e1ccc126884938c64e1d
mishamisha123414 <misha.melnichenko.80@gmail.com>
2024-02-09 16:49:34 +0200

    09.02.24

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ root.render(
   </GoogleOAuthProvider>,
   document.getElementById('root')
 );
+console.log ('OK');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
